1eda1d7d4c1946c1cd3dfe4ed4e46860556c3a4b
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2015-12-10 09:22:37 -0600

    views added but not routed

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 
 adminRouter.get('/*', function(req, res){
-    var file = req.params[0] || "assets/views/admin.html";
+    var file = req.params[0] || "assets/views/login.html";
     res.sendFile(path.join(__dirname, "../public/", file))
 });
 
