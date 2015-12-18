8c3b306df3c7a8fd9284854148f87adc67d80964
karlhass15 <karl@reclaimedartcraftsman.com>
2015-12-18 11:13:58 -0600

    another models

--- server/routes/addStore.js
+++ server/routes/addStore.js
@@ -1,7 +1,7 @@
 //Variable declarations
 var express = require('express');
 var router = express.Router();
-var Store = require('../models/store');
+var Store = require('./store');
 
 //Adding a new store to the Database
 router.post('/', function(req,res){
--- server/routes/categoryList.js
+++ server/routes/categoryList.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Store = require('../models/store');
+var Store = require('./store');
 
 
 router.get('/', function(req, res){
