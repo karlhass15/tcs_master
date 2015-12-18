8242c67c4fa01bfdfcd2ac5e9ac0e86d15803410
colinmiller <colinchmiller@gmail.com>
2015-12-18 11:03:46 -0600

    prior to remote merge after remote error fix

--- server/routes/categoryList.js
+++ server/routes/categoryList.js
@@ -5,8 +5,7 @@ var Store = require('../models/store');
 
 router.get('/', function(req, res){
     console.log("Here is the req.query: ", req.query);
-    Store.aggregate({$geoNear: {near: [parseFloat(req.query.lng), parseFloat(req.query.lat)], distanceField: "distance",
-            query : {categories : {$elemMatch : {category: req.query.category}}}, spherical: true, limit: 3 }},
+    Store.find({_id : {$in : req.query.paramArray}},
         function(err, data){
             if (err) {
                 console.log("Error in the query!: ", err);
